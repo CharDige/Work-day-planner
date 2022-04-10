e02e71c13e8bf2edb1c026ac80f82bae82bd1714
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-04-10 11:42:40 +0930

    Initial code for local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 // Current date
 var currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
+
+// Save button click event
+var saveBtn = $(".saveBtn")
+var scheduleInput = $(".description");
+saveBtn.on("click", function() {
+    localStorage.setItem("scheduleInput", scheduleInput.val());
+});
